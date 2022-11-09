6df9344849bf9f8218e9cbf17b172ff0396bb8e0
VincenzoCastrignano <vincenzo.castrignano@esqese.ucly.fr>
2022-11-09 16:30:34 +0100

    Ajout Login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-/*
+import { initializeApp } from "firebase/app";
+import firebaseConfig from './config.js';
+import logo from './logo.svg';
+import './App.css';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, updateCurrentUser } from "firebase/auth";
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 function App() {
+  const [user] = useAuthState(auth);
   return (
-    <div>
-      <h1>
-        Hello world
-      </h1>
+    <div className="App">
+        <header>
+          <h1>
+            chatoubadi <img src={logo} alt="logo"/>
+          </h1>
+          <LogOut />
+        </header>
+        <section>
+          {user ?
+            <Chatroom /> : <Login />
+          }
+        </section>
     </div>
   );
 }
-*/
 
-import logo from './logo.svg';
-import './App.css';
+const Login = () => {
 
-const App = () => {
+  const Login_Google = () => {
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider);
+    
+  }
   return (
-    <h1>Hello world</h1>
+    <>
+    <button onClick={Login_Google}>Login</button>
+    </>
   )
 }
+
+const LogOut = () => {
+  const LogOut_Google = () => {
+    signOut(auth)
+  }
+    return (
+      auth.currentUser && 
+      <>
+        <button onClick={LogOut_Google}>Disconnect</button>
+      </>
+      )
+  }
+
+const Chatroom = () => {
+  return (
+    <h3>Chatroom</h3>
+  )
+}
+
+
+
 
 export default App;
